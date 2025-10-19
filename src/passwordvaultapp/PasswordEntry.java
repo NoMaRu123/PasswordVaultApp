e95696bd468d45567b499f25a75b0a9e9c023ff4
Alex MacHugh <alex.t.machugh@gmail.com>
2025-10-19 05:11:07 +0100

    added User and PasswordEntry classses with all the getters and setters it need for the future update

--- src/passwordvaultapp/PasswordEntry.java
+++ src/passwordvaultapp/PasswordEntry.java
@@ -4,10 +4,82 @@
  */
 package passwordvaultapp;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Date;
+
 /**
  *
  * @author ASUS
  */
 public class PasswordEntry {
+    private int entryID;
+    private int userID; // Foreign key to User
+    private String serviceName;
+    private String username; // username for that service
+    private byte[] encryptedPassword;
+    private Date creationDate;
+
+    public PasswordEntry(int entryID, int userID, String serviceName, String username, byte[] encryptedPassword, Date creationDate) {
+        this.entryID = entryID;
+        this.userID = userID;
+        this.serviceName = serviceName;
+        this.username = username;
+        this.encryptedPassword = encryptedPassword;
+        this.creationDate = creationDate;
+    }
     
+    public int getEntryID() {
+        return entryID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public byte[] getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String decryptPassword(EncryptionService encryptionService, byte[] key){
+        return encryptionService.decrypt(this.encryptedPassword, key);
+    }
+
+    public void encryptdPassword(EncryptionService encryptionService, String plainPassword, byte[] key) {
+        this.encryptedPassword = encryptionService.encrypt(plainPassword, key);
+    }
+    
+    // for debugging
+    
+    @Override
+    public String toString() {
+        return "PasswordEntry{" +
+                ", entryID=" + entryID +
+                ", userID=" + userID +
+                ", serviceName=" + serviceName +'\'' +
+                ", username=" + username + '\'' +
+                ", encryptedpassword=" + Arrays.toString(encryptedPassword) +
+                ", creationDate=" + creationDate +
+                '}';
+    }
 }
--- src/passwordvaultapp/User.java
+++ src/passwordvaultapp/User.java
@@ -4,10 +4,58 @@
  */
 package passwordvaultapp;
 
+import java.util.Arrays;
+
 /**
  *
  * @author ASUS
  */
 public class User {
+    private int userID;
+    private String username;
+    private byte[] hashedPassword;
+    private byte[] salt;
+    
+    public User(int userID, String username, byte[] hashedPassword, byte[] salt){
+        this.salt = salt;
+        this.userID = userID;
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+    }
+    
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public byte[] getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(byte[] hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+   // for debugging
+    public String toString() {
+        return "User{" + 
+                "userID=" + userID +
+                ", username=" + username + '\'' +
+                ", hashedPassword=" + Arrays.toString(hashedPassword) +
+                ", salt=" + Arrays.toString(salt) + '}';
+    }
+    
     
 }
+
